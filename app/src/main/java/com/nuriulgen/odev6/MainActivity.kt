8f35df622a5bb54ff7e5f8fba831c33ddebe3d15
Nuri Ulgen <nuriulgen04400@gmail.com>
2022-10-01 22:55:22 +0300

    project is completed.

--- app/src/main/java/com/nuriulgen/odev6/MainActivity.kt
+++ app/src/main/java/com/nuriulgen/odev6/MainActivity.kt
@@ -2,10 +2,30 @@ package com.nuriulgen.odev6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter= 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        while(true){
+            CoroutineScope(Dispatchers.IO).launch {
+                val answer = doNetworkCall()
+                withContext(Dispatchers.Main){
+                    Log.v("PATIKA",answer)
+                }
+                counter++
+                println("Counter: $counter")
+            }
+        }
+
+    }
+    private suspend fun doNetworkCall(): String {
+        delay(2000L)
+        return "Network Answer Called"
     }
 }
